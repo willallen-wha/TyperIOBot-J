728467d4b1b1f8795c55b6b37d9d767ebedea6a4
Will Allen <willallen.wha@gmail.com>
2024-06-12 19:06:45 -0600

    Removed unused imports & better constructor

--- TyperIOBot-J/EnhancedRobot.java
+++ TyperIOBot-J/EnhancedRobot.java
@@ -2,30 +2,36 @@ import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
+// import java.awt.image.BufferedImage;
+// import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.HashMap;
-import javax.imageio.ImageIO;
-import java.awt.Rectangle;
-import java.awt.Toolkit;
-import java.awt.MouseInfo;
+// import javax.imageio.ImageIO;
+// import java.awt.Rectangle;
+// import java.awt.Toolkit;
+// import java.awt.MouseInfo;
 
 public class EnhancedRobot extends Robot{
 
-    final Rectangle screen;
+    // final Rectangle screen;
     final String dumpFileName;
     private int typeSpeedConst;
 
-    public EnhancedRobot(String dump) throws AWTException {
+    public EnhancedRobot(String dump, int speed, int delay) throws AWTException {
         super();
-        int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
-        int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
-        screen = new Rectangle(0, 0, screenWidth, screenHeight);
+        // int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+        // int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+        // screen = new Rectangle(0, 0, screenWidth, screenHeight);
+        // File where the raw data will be dumped
         dumpFileName = dump;
-        typeSpeedConst = 45;
+        // Speed between letters during typing
+        typeSpeedConst = speed;
+        // Speed between all actions including alt-tabs, etc.
+        setAutoDelay(delay);
+        // Initialize the decoder with a blank keycode request
+        Decoder.getKeycode(' ');
     }
 
     //Press and release a key instantly
@@ -63,23 +69,20 @@ public class EnhancedRobot extends Robot{
         }
     }
 
+    // Custom delay function to allow random time between letter presses
+    // This theoretically makes the botting less obvious.
     public void customDelay() {
+        // If no delay is requested simply continue
         if(typeSpeedConst == 0) {
             return;
         }
-        //Delay a custom amount of time to make it seem less like botting
-        //Create a skew factor between 0 and half of the delay time
+        // Create a skew factor between 0 and half of the delay time
         int adjust = 1;
         adjust *= (Math.random() * (typeSpeedConst / 2));
-        //Determine if skew is positive or negative
+        // Determine if skew is positive or negative
         if(Math.random() * 2 > 1) adjust *= -1;
-        //Adjust by skew, rounding up to 0 if below 0
-        try {
-            delay(typeSpeedConst + adjust);
-        }
-        catch (IllegalArgumentException e) {
-            delay(0);
-        }
+        // Adjust by skew and delay
+        delay(typeSpeedConst + adjust);
         System.out.println("Delayed for " + (typeSpeedConst + adjust) + " milliseconds.");
     }
 
@@ -91,6 +94,7 @@ public class EnhancedRobot extends Robot{
         keyRelease(KeyEvent.VK_ALT);
     }
 
+    //Simulates multiple alt-tab actions
     public void altTab(int times) {
         keyPress(KeyEvent.VK_ALT);
         while(times > 0) {
@@ -101,43 +105,46 @@ public class EnhancedRobot extends Robot{
         keyRelease(KeyEvent.VK_ALT);
     }
 
-    public BufferedImage takeScreenshot() {
-        BufferedImage screenshot = createScreenCapture(screen);
-        //Write the screenshot to a file
-        try {
-            File out = new File("screen.jpg");
-            ImageIO.write(screenshot, "jpg", out);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        return screenshot;
-    }
+    // public BufferedImage takeScreenshot() {
+    //     BufferedImage screenshot = createScreenCapture(screen);
+    //     //Write the screenshot to a file
+    //     try {
+    //         File out = new File("screen.jpg");
+    //         ImageIO.write(screenshot, "jpg", out);
+    //     } catch(Exception e) {
+    //         e.printStackTrace();
+    //     }
+    //     return screenshot;
+    // }
 
-    public BufferedImage takeScreenshotFromMouse() {
-        Rectangle toCapture = new Rectangle(getMouseX(), getMouseY(), 750, 750);
-        BufferedImage screenshot = createScreenCapture(toCapture);
-        //Write the screenshot to a file
-        try {
-            File out = new File("screen.jpg");
-            ImageIO.write(screenshot, "jpg", out);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        return screenshot;
-    }
+    // public BufferedImage takeScreenshotFromMouse() {
+    //     Rectangle toCapture = new Rectangle(getMouseX(), getMouseY(), 750, 750);
+    //     BufferedImage screenshot = createScreenCapture(toCapture);
+    //     //Write the screenshot to a file
+    //     try {
+    //         File out = new File("screen.jpg");
+    //         ImageIO.write(screenshot, "jpg", out);
+    //     } catch(Exception e) {
+    //         e.printStackTrace();
+    //     }
+    //     return screenshot;
+    // }
 
-    public int getMouseX() {
-        return (int) MouseInfo.getPointerInfo().getLocation().getX();
-    }
+    // public int getMouseX() {
+    //     return (int) MouseInfo.getPointerInfo().getLocation().getX();
+    // }
 
-    public int getMouseY() {
-        return (int) MouseInfo.getPointerInfo().getLocation().getY();
-    }
+    // public int getMouseY() {
+    //     return (int) MouseInfo.getPointerInfo().getLocation().getY();
+    // }
 
     public void initiate() throws FileNotFoundException {
-        //Wait a seconds to allow mouse to be placed
+        // Wait a seconds to allow mouse to be placed
         delay(1000);
-        //Now, right click and rip the HTML
+        // Now, right click and rip the HTML
+        // Runs the series of keystrokes right-click, C, C,
+        // Enter, Enter, Enter to navigate the right click menu to
+        // "Copy Element" option and get just Gameboard_container data
         mouseClick(InputEvent.getMaskForButton(3));
         delay(60);
         keyType(KeyEvent.VK_C);
@@ -151,11 +158,14 @@ public class EnhancedRobot extends Robot{
         delay(15);
         keyType(KeyEvent.VK_ENTER);
         delay(15);
-        //Now that we've got it ripped, put it in the text file
-        //Alt-tab into the open text file
+        // Now that we've got it ripped, put it in the text file
+        // Alt-tab into the open text file which should be "logically" behind
+        // two alt-tabs: the first is whatever is running the code, the second
+        // reaches the text file
         altTab(2);
         delay(25);
-        //Now paste and save
+        // Now paste and save by selecting all, backspacing, pasting,
+        // and saving the text file.
         all();
         delay(25);
         keyType(KeyEvent.VK_BACK_SPACE);
@@ -164,46 +174,43 @@ public class EnhancedRobot extends Robot{
         delay(25);
         save();
         delay(25);
-        //Now that it's been saved, parse that HTML and refocus the game window
+        // Now that it's been saved, parse that HTML and refocus the game window
         altTab(1);
         String toType = RawParser.parseRaw(dumpFileName).trim();
         System.out.println("String to be typed: " + toType);
-        //Let the mouse get situated then get ready to go
+        // Let the mouse get situated then get ready to go
         delay(1500);
         mouseClick(InputEvent.getMaskForButton(1));
         delay(10);
-        //Type a space, wait 3 seconds for the round to start, then let er rip
+        // Type a space, wait 3 seconds for the round to start, then let er rip
         keyType(' ');
+        // If you're trying to do anything other than the solo text snippets,
+        // change this number to be the right length.
         delay(4500);
         typeString(toType);
     }
 
+    // Shorthand function for pressing control and V to paste
     public void paste() {
         keyPress(KeyEvent.VK_CONTROL);
         keyType(KeyEvent.VK_V);
         keyRelease(KeyEvent.VK_CONTROL);
     }
 
+    // Shorthand function for pressing control and S to save
     public void save() {
         keyPress(KeyEvent.VK_CONTROL);
         keyType(KeyEvent.VK_S);
         keyRelease(KeyEvent.VK_CONTROL);
     }
 
+    // Shorthand function for pressing control and A to select all
     public void all() {
         keyPress(KeyEvent.VK_CONTROL);
         keyType(KeyEvent.VK_A);
         keyRelease(KeyEvent.VK_CONTROL);
     }
 
-    public void ensureSet() {
-        Decoder.getKeycode(' ');
-    }
-
-    public void setTypeSpeed(int speed) {
-        typeSpeedConst = speed;
-    }
-    
 }
 
 class Decoder {
--- TyperIOBot-J/Main.java
+++ TyperIOBot-J/Main.java
@@ -2,23 +2,25 @@ public class Main {
     
     public static void main(String[] args) {
 
+        // Location and name of file that stores raw HTML data ripped from the site.
         String nameOfFile = "raw.txt";
+        /*
+         * Base time, in miliseconds, between each keypress before randomization. 
+         * Setting to 0 results in no delay between keypresses, any number higher
+         * will result in a uniform distribution of X length centered at X. E.G.:
+         * 60 will create random waits between 30 and 90 ms (60ms wide centered at 60)
+        */
+        int typingSpeed = 20;
+        /*
+         * The amount of time, in milliseconds, between every action the robot takes.
+         * This includes alt-tabs, keydown vs keyup events, everything. The recommended
+         * value is 0, but can be increased for an even slower robot.
+         */
+        int robotDelay = 0;
 
         try {
             //New robot object for virtual keyboard
-            EnhancedRobot virt = new EnhancedRobot(nameOfFile);
-            //Make sure mapping of characters to keycodes is set
-            virt.ensureSet();
-            //Set specific variables:
-            //The default time, in miliseconds, between each keypress. Setting to 0
-            //results in no random delay between keypresses, any number higher will
-            //result in a uniform distribution of X length centered at X. For example,
-            //setTypeSpeed(60) will create random waits between 30 and 90 ms (60ms wide
-            //centered at 60)
-            virt.setTypeSpeed(20);
-            //Amount of time the bot takes between each action, i.e. each individual key
-            //down and key up event. 0 recommended, but can be increased to avoid missed strokes.
-            virt.setAutoDelay(0);
+            EnhancedRobot virt = new EnhancedRobot(nameOfFile, typingSpeed, robotDelay);
             
             virt.initiate();
 
